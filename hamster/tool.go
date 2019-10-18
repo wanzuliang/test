4262a3b7a07d97d9be6a52fd2676a85064a34ab4
wanzuliang <963789681@qq.com>
2019-10-18 17:45:57 +0800

    m

--- hamster/tool.go
+++ hamster/tool.go
@@ -4,79 +4,100 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	// "reflect"
 )
 
 func main() {
+
 	var list [] int
-	var list2 [] int
+
+	list_add(&list, 10000, 0, 10000)
+	// list_show(&list)
+
+
+	// T := sort_insert(&list)
+	// T := sort_bubble(&list)
+	T := sort_select(&list)
 	
-	list_add(&list, 10000, 0, 9999)
-	list_add(&list2, 10000, 0, 9999)
+	fmt.Println(T)
 
-	sort_insert(&list)
-	sort_bubble(&list2)
-
-	// fmt.Println(list)
-
-	// for _, value := range *list {
-	// 	if true || value == 20 || value == 30{
-	// 		fmt.Print(value, " ")
-	// 	}
+	// list_show(&list)
+}
+func list_show(list *[]int) {
+	var L = *list
+	fmt.Println(L)
+	// for k, value := range *list  {
+	// 	fmt.Println(k, ":" , value)
 	// }
-	for i := 0; i < 100; i++ {
-		fmt.Print(list[i] , " ")
-	}
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	for i := 0; i < 100; i++ {
-		fmt.Print(list2[i] , " ")
-	}
 }
 
 func list_add(list *[]int, len, min, max int) {
+	//
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < len; i++ {
 		*list = append(*list, rand.Int() % (max-min+1)+min)
 	}
 }
 
-func sort_insert(list *[]int) {
-	len := len(*list)
+func sort_insert(list *[]int) time.Duration{
+	//
+	var L = *list
+	len := len(L)
+
+	start := time.Now()
 	for i := 1; i < len; i++ {
 		for j := 0; j < i; j++ {
-			if (*list)[i] < (*list)[j] {
-				temp := (*list)[i]
+			if L[i] < L[j] {
+				temp := L[i]
 				for k := i; k > j; k-- {
-					(*list)[k] = (*list)[k-1]
+					L[k] = L[k-1]
 				}
-				(*list)[j] = temp
+				L[j] = temp
 			}
 		}
 	}
-
-	// for i := 0; i < len; i++ {
-	// 	fmt.Print( (*list)[i] ," AAA ")
-	// }
+	end := time.Now()
+	return end.Sub(start)
 }
 
-func sort_bubble(list *[]int) {
-	len := len(*list)
+func sort_bubble(list *[]int) time.Duration{
+	//
+	var L = *list
+	len := len(L)
+
+	start := time.Now()
 	for i := 0; i < len; i++ {
-		min := (*list)[i]
+		min := L[i]
 		num := i
 		
 		for j := i+1; j < len; j++ {
-			if (*list)[j] < min {
-				min = (*list)[j]
+			if L[j] < min {
+				min = L[j]
 				num = j
 			}
 		}
-		(*list)[num] = (*list)[i]
-		(*list)[i] = min
+		L[num] = L[i]
+		L[i] = min
 	}
+	end := time.Now()
+	return end.Sub(start)
+}
 
-	// for i := 0; i < len; i++ {
-	// 	fmt.Print( (*list)[i] ," BBB ")
-	// }
+func sort_select(list *[]int) time.Duration{
+	//
+	L := *list
+	len := len(L)
+
+	start := time.Now()
+	for i := 1; i < len; i++ {
+		for j := 0; j < i; j++ {
+			if L[i] < L[j] {
+				temp := L[j]
+				L[j] = L[i]
+				L[i] = temp
+			}
+		}
+	}
+	end := time.Now()
+	return end.Sub(start)
 }
